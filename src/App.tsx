575ff25210d0273e668fa465b9479c5e60cdbc8c
ryutaro1069@gmail.com <ryutaro1374@icloud.com>
2021-04-20 13:53:58 +0900

    react-tutorial-tsのコード部分

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,142 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+interface SquareProps {
+	value: string | null;
+	onClick: () => void;
 }
 
-export default App;
+const Square: React.FC<SquareProps> = ({ value, onClick }) => {
+	return (
+		<button className="square" onClick={() => onClick()}>
+			{value}
+		</button>
+	);
+};
+
+interface BoardProps {
+	squares: ("O" | "X" | null)[];
+	onClick: (i: number) => void;
+}
+
+const Board: React.FC<BoardProps> = ({ squares, onClick }) => {
+	const renderSquare = (i: number) => {
+		return <Square value={squares[i]} onClick={() => onClick(i)} />;
+	};
+
+	return (
+		<div>
+			<div className="board-row">
+				{renderSquare(0)}
+				{renderSquare(1)}
+				{renderSquare(2)}
+			</div>
+			<div className="board-row">
+				{renderSquare(3)}
+				{renderSquare(4)}
+				{renderSquare(5)}
+			</div>
+			<div className="board-row">
+				{renderSquare(6)}
+				{renderSquare(7)}
+				{renderSquare(8)}
+			</div>
+		</div>
+	);
+};
+
+interface HistoryData {
+	squares?: ("O" | "X" | null)[];
+}
+
+interface GameState {
+	history?: HistoryData[];
+	xIsNext?: boolean;
+	stepNumber?: number;
+}
+
+const Game: React.FC<GameState> = () => {
+	const [state, setState] = useState({
+		history: [{ squares: Array(9).fill(null) }],
+		stepNumber: 0,
+		xIsNext: true,
+	});
+	const handleClick = (i: number) => {
+		const newHistory = state.history.slice(0, state.stepNumber + 1);
+		const current = newHistory[newHistory.length - 1];
+		const squares = current.squares.slice();
+		if (calculateWinner(squares) || squares[i]) {
+			return;
+		}
+
+		squares[i] = state.xIsNext ? "X" : "O";
+		setState({
+			history: newHistory.concat([
+				{
+					squares: squares,
+				},
+			]),
+			stepNumber: newHistory.length,
+			xIsNext: !state.xIsNext,
+		});
+	};
+
+	const jumpTo = (move: number) => {
+		setState({
+			...state,
+			stepNumber: move,
+			xIsNext: move % 2 === 0,
+		});
+	};
+	const current = state.history[state.stepNumber];
+	const winner = calculateWinner(current.squares);
+	const moves = state.history.map((_, move) => {
+		const desc = move ? "Go to move #" + move : "Go to game start";
+		return (
+			<li key={move}>
+				<button onClick={() => jumpTo(move)}>{desc}</button>
+			</li>
+		);
+	});
+	let status;
+	if (winner) {
+		status = "Winner: " + winner;
+	} else {
+		status = "Next player: " + (state.xIsNext ? "X" : "O");
+	}
+
+	return (
+		<div className="game">
+			<div className="game-board">
+				<Board squares={current.squares} onClick={(i) => handleClick(i)} />
+			</div>
+			<div className="game-info">
+				<div>{status}</div>
+				<ol>{moves}</ol>
+			</div>
+		</div>
+	);
+};
+const calculateWinner = (squares: any) => {
+	const lines = [
+		[0, 1, 2],
+		[3, 4, 5],
+		[6, 7, 8],
+		[0, 3, 6],
+		[1, 4, 7],
+		[2, 5, 8],
+		[0, 4, 8],
+		[2, 4, 6],
+	];
+	for (let i = 0; i < lines.length; i++) {
+		const [a, b, c] = lines[i];
+		if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+			return squares[a];
+		}
+	}
+	return null;
+};
+
+export default Game;
